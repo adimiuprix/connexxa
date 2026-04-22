ba7fa5f611c135631e0f3c1f09150c546398c012
adimiuprix <miuprix@gmail.com>
2026-04-22 11:13:30 +0700

    feat: initialize project layout with Tailwind CSS configuration and global typography styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oswald } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
+const oswald = Oswald({
+    variable: "--font-oswald",
     subsets: ["latin"],
 });
 
@@ -28,7 +23,7 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+            className={`${oswald.variable} h-full antialiased`}
         >
             <body className="min-h-full flex flex-col">
                 <Header />
